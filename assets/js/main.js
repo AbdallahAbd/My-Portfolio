fb0064c5f49c52a1c556f26b3c9c57c031c68af3
AbdallahAbd <147553756+AbdallahAbd@users.noreply.github.com>
2023-11-01 02:21:07 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,12 +186,12 @@ const selectedIcon = localStorage.getItem("selected-icon");
 
 // obtain the current theme
 const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
+  document.body.classList.contains(darkTheme) ? "light" : "dark";
 const getCurrentIcon = () =>
   themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
 if (selectedTheme) {
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+  document.body.classList[selectedTheme === "light" ? "add" : "remove"](
     darkTheme
   );
   themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
